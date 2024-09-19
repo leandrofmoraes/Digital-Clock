18201fb658594d73aef5e8cf2713e96d53f0756e
leandrofmoraes <leandrofmraes@gmail.com>
2024-09-19 19:17:59 -0300

    Simplified HTML file and renamed js file

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
-const clock = document.querySelector('#disp');
+"use strict"
+
+const clock = document.querySelector("section");
 window.addEventListener("load", relogio);
 
 function relogio(){
@@ -7,22 +8,11 @@ function relogio(){
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-=======
-"use strict"
-
-let clock = document.querySelector('#disp');
-window.addEventListener("load", relogio);
-
-function relogio(){
-    let data = new Date();
-    let hours = data.getHours();
-    let minutes = data.getMinutes();
-    let seconds = data.getSeconds();
->>>>>>> 81aaa2f49b68bccbaa4f9c2c0a2ed8d4901c82d3
 
     if (hours < 10) hours=`0${hours}`;
     if (minutes < 10) minutes=`0${minutes}`;
     if (seconds < 10) seconds=`0${seconds}`;
+
     clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
